742a2594521a5e2f8248d111f0dd2ebd10b7ca24
XU PU <ptx.pluto@gmail.com>
2021-07-27 19:14:27 +0800

    parallel

--- train_rllib.py
+++ train_rllib.py
@@ -3,20 +3,20 @@ from ray import tune
 from ray.rllib.agents.ppo import PPOTrainer
 from ray.rllib.agents.sac import SACTrainer
 from rock_walk.envs.motion_control_rnw_env import MotionControlRnwEnv
-
+import ray
+from ray import tune
 
 if __name__ == "__main__":
-    ray.init()  # Skip or set to ignore if already called
-    config = {
-        "framework": "torch",
-        'simple_optimizer': True,
-        # 'gamma': 0.9,
-        # 'lr': 1e-2,
-        'num_workers': 4,
-        # 'train_batch_size': 1000,
-        #'model': {'fcnet_hiddens': [128, 128]}
-    }
-    trainer = SACTrainer(env=MotionControlRnwEnv, config=config)
-    results = trainer.train()
-    print(results)
-    #tune.run(PPOTrainer, config={"env": , })
+
+    ray.init()
+    tune.run(
+        "SAC",
+        stop={"timesteps_total": 1e6},
+        config={
+            "framework": "torch",
+            "env": MotionControlRnwEnv,
+            "rollout_fragment_length": 100,
+            "num_gpus": 1,
+            "num_workers": 6
+        },
+    )
